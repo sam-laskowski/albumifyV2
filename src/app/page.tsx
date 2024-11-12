1b8b6dad7caf3ee98d6342c2d54c8642dfe4daa7
samelpo <sam.laskowski@outlook.com>
2024-11-12 01:07:24 +0000

    added logout button and react icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { useAuth } from "@/context/AuthContext";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -7,13 +8,23 @@ import { useState } from "react";
 
 export default function Page() {
   const router = useRouter();
+
+  const { globalUser, logout } = useAuth();
   return (
     <>
-      <h1 className="text-white font-extrabold text-3xl cursor-pointer">
-        albumify
-      </h1>
+      <header className="flex flex-row justify-between">
+        <h1 className="text-white font-extrabold text-3xl cursor-pointer">
+          albumify
+        </h1>
+        <div className="flex flex-row gap-2">
+          <Link href="/login">
+            <Button>Login</Button>
+          </Link>
+
+          {globalUser && <Button onClick={logout}>Logout</Button>}
+        </div>
+      </header>
       <Button onClick={() => router.push("/overview")}>Explore</Button>
-      <Link href="/login">Login</Link>
     </>
   );
 }
